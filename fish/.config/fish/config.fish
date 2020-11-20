3763d12aecbcfcddcffaea9d6d789aab9d14dec4
khhhhh <oleg.hutsko@gmail.com>
2020-11-20 15:34:06 +0100

    some changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 fish_vi_key_bindings
 
+
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
@@ -9,3 +10,7 @@ alias vimi3='nvim ~/.config/i3/config'
 alias neofetch='neofetch --w3m'
 alias ranger='ranger --choosedir=$HOME/.config/ranger/rangerdir; set LASTDIR (cat $HOME/.config/ranger/rangerdir); cd "$LASTDIR"'
 alias vim='nvim'
+set -Ux XDG_CONFIG_HOME $HOME/.config
+set -Ux XDG_CACHE_HOME $HOME/.cache
+set -Ux XDG_DATA_HOME $HOME/.local/share
+set fish_greeting
